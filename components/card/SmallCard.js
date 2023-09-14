5d14bcb16b5602179dfe3c194121f294e2d03733
daria.farina <dashafarina25@gmail.com>
2023-09-14 18:56:26 +0300

    fixes

--- components/card/SmallCard.js
+++ components/card/SmallCard.js
@@ -4,15 +4,15 @@ import Image from "next/image"
 
 const SmallCard = ({
     title,
-    imageSrc,
-    description : propDescription
+    image,
+    description
 }) => {
-    const [show_read_more, setReadMore] = useState(propDescription.length > 230);
-    const [fullDescription] = useState(propDescription);
+    const [show_read_more, setReadMore] = useState(description.length > 230);
+    const [fullDescription] = useState(description);
     const [displayDescription, setDisplayDescription] = useState(
-        propDescription.length > 230 ?
-            propDescription.substring(0, 230-3) + '...' :
-            propDescription
+        description.length > 230 ?
+            description.substring(0, 230-3) + '...' :
+            description
     );
 
     const read_more = () => {
@@ -23,7 +23,7 @@ const SmallCard = ({
     return (
         <div className="sm-card text-center max-w-[335px] md:mx-2.5 mb-10 md:mb-0 rounded-[20px]">
             <div className="sm-card__image rounded-[20px] overflow-hidden h-[223.88px] flex items-center justify-center relative">
-                <Image src={imageSrc} alt={title} layout="fill" className="object-cover" />
+                <Image src={image.src} layout="fill" className="object-cover" alt={image.alt} />
             </div>
             { (title || description) && (
                 <div className="sm-card__content -mt-5 pt-16 pb-[20px] px-[28px] border-2 border-dark rounded-b-[20px] bg-white">
@@ -32,7 +32,7 @@ const SmallCard = ({
                             <h3 className="text-[40px] font-heading font-normal text-dark leading-110 mb-6">{title}</h3>
                         </div>
                     )}
-                    {propDescription && (
+                    {description && (
                         <div className="sm-card__description text-black ...">
                             <p>{displayDescription}</p>
                         </div>
--- components/sections/CertifiedSection.js
+++ components/sections/CertifiedSection.js
@@ -3,7 +3,7 @@ import CertifiedSlider from "@/components/CertifiedSlider"
 
 const CertifiedSection = ( {title} ) => {
     return (
-        <div className="w-[1167px] mx-auto flex justify-between items-center">
+        <div className="w-[1204px] pr-[20px] pl-[20px] mx-auto flex justify-between items-center">
             <div className="certified__title">
                 <h3>{title}</h3>
             </div>
