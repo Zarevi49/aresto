99331ed498e43f7c28e45adb119790aaba9c6149
Zarevi4 <zarevi4@gmail.com>
2023-09-15 11:34:55 +0300

    btn form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -15,9 +15,9 @@ function ContactForm() {
                 </address>
             </div>
 
-            <div className="flex flex-col w-full max-w-[520px]">
+            <form className="flex flex-col w-full max-w-[520px]">
                 <div className="mb-[9px]">
-                    <select id="topic" className="w-full border-2 border-dark rounded-full px-[21px] py-[5px] h-[44px] placeholder:text-dark text-dark font-normal text-[15px]">
+                    <select id="topic" className="w-full border-2 border-dark focus:border-dark rounded-full px-[21px] py-[5px] h-[44px] outline-none placeholder:text-dark text-dark font-normal text-[15px]">
                         <option value="" disabled selected hidden>Choose a topic</option>
                         <option value="topic1">Topic 1</option>
                         <option value="topic2">Topic 2</option>
@@ -39,9 +39,15 @@ function ContactForm() {
                 </div>
 
                 <div className="text-right">
-                    <Button link="#" text="Send" showArrow={true} accent={true} />
+                    <button type={"submit"} className={`bg-blue-500 text-dark py-1 px-2.5 rounded-full text-[13px] font-normal font-lato font-medium leading-[19.5px] tracking-[1.3px] uppercase border-2 cursor-pointer hover:bg-primary-light bg-ease-in-out duration-300 inline-flex align-center bg-primary-light hover:bg-white`}>
+                        Send
+                            <svg width="17" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-2.5">
+                                <path d="M8.48212 1.12891L15.982 8.6288L8.48212 16.1287" stroke="#1D1D1D" stroke-width="1.5" stroke-miterlimit="10"/>
+                                <path d="M15.982 8.62866H0.982239" stroke="#1D1D1D" stroke-width="1.5" stroke-miterlimit="10"/>
+                            </svg>
+                    </button>
                 </div>
-            </div>
+            </form>
         </div>
     );
 }
